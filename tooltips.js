8f4f279fe43b241b8b26262936c59139c43d158c
Jacek Jarczok <kson.eu@gmail.com>
2021-01-28 18:09:41 +0100

    wip

--- tooltips.js
+++ tooltips.js
@@ -6,9 +6,9 @@ const tooltipParagraph = document.querySelector('.tooltip-text');
 /* tooltip texts */
 const tooltipTexts = {
   1: "First text",
-  2: "Second example",
+  2: "Second example. Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, dolor fugiat?",
   3: "Just another simple text",
-  4: "Ok, last example"
+  4: "Ok, last quite long example. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam atque amet dolorum esse. Totam quis doloremque quae rerum corporis, beatae eum asperiores officiis consequuntur. Aliquam, tenetur dolorum eius eveniet nesciunt quos, nam, quisquam porro harum iste esse quod maiores! Molestiae ea atque facilis aperiam ratione impedit adipisci, necessitatibus recusandae. Vel ut temporibus alias sequi harum commodi reprehenderit, voluptate pariatur itaque omnis vero? Accusantium quia consequuntur nobis maxime adipisci reiciendis minima vel rerum quam architecto dolore saepe eveniet, non hic obcaecati iure animi quo esse vero. Perferendis, quibusdam minus?"
 }
 
 /* .tooltip-btn events */
