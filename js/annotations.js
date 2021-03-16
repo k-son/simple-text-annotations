9bb8a88e4f137202def9da71deec53d313e89bd4
Jacek Jarczok <kson.eu@gmail.com>
2021-03-16 08:55:20 +0100

    Refactor and simplyfy script

--- js/annotations.js
+++ js/annotations.js
@@ -1,12 +1,12 @@
 (function() {
   "use strict";
-  
+
   /* variables */
   const annotationBtn = document.querySelectorAll('.annotation-btn');
   const annotationWrapper = document.querySelector('.annotation-wrapper');
   const annotationCloseBtn = document.querySelector('.annotation-close-btn');
   const annotationParagraph = document.querySelector('.annotation-text');
-  
+
   /* put here your annotations texts */
   const annotationTexts = {
     text1: "First text example.",
@@ -14,51 +14,44 @@
     text3: "<ul style='list-style-type: none;'><li>First list item.</li><li>Second list item.</li><li>Third list item.</li>",
     text4: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam atque amet dolorum esse. Totam quis doloremque quae rerum corporis, beatae eum asperiores officiis consequuntur. Aliquam, tenetur dolorum eius eveniet nesciunt quos, nam, quisquam porro harum iste esse quod maiores! Molestiae ea atque facilis aperiam ratione impedit adipisci, necessitatibus recusandae. Vel ut temporibus alias sequi harum commodi reprehenderit, voluptate pariatur itaque omnis vero? Accusantium quia consequuntur nobis maxime adipisci reiciendis minima vel rerum quam architecto dolore saepe eveniet, non hic obcaecati iure animi quo esse vero. Perferendis, quibusdam minus?"
   }
-  
+
   /* .annotation-btn events */
   annotationBtn.forEach(function(button) {
     button.addEventListener('click', function() {
-      const prop = this.dataset.text;
-      const text = annotationTexts[prop];
-      // opener indicates which button has launched the annotation
-      const opener = annotationWrapper.dataset.opener;
-  
-      if (opener === 'none') {
-        annotationParagraph.innerHTML = text;
-        this.setAttribute('aria-describedby', 'annotation-text');
-        annotationWrapper.dataset.opener = this.id;
-        annotationWrapper.classList.add('show-annotation');
-        this.classList.add('annotation-btn--active');
-  
-      } else if (opener === this.getAttribute('id')) {
+
+      // check if this button was used to open the annotation-wrapper
+      if (this.classList.contains('annotation-btn--active')) {
         annotationWrapper.classList.remove('show-annotation');
         setTimeout(function() {
           annotationParagraph.innerHTML = '';
         }, 300);
-        this.setAttribute('aria-describedby', '');
-        annotationWrapper.dataset.opener = 'none';
         this.classList.remove('annotation-btn--active');
-        
-      } else if ((opener !== annotationWrapper.getAttribute('id') && (opener !== 'none'))) {
+        this.setAttribute('aria-describedby', '');
+
+      } else {
+        const text = annotationTexts[this.dataset.text];
+        annotationBtn.forEach(function(btn) {
+          btn.classList.remove('annotation-btn--active');
+          btn.setAttribute('aria-describedby', '');
+        })
         annotationParagraph.innerHTML = text;
-        annotationBtn.forEach(function(btn) {btn.setAttribute('aria-describedby', '')});
-        this.setAttribute('aria-describedby', 'annotation-text');
-        annotationWrapper.dataset.opener = this.id;
-        annotationBtn.forEach(function(btn) {btn.classList.remove('annotation-btn--active')});
+        annotationWrapper.classList.add('show-annotation');
         this.classList.add('annotation-btn--active');
+        this.setAttribute('aria-describedby', 'annotation-text');
       }
     })
   })
-  
+
   /* .annotation-close-btn events */
   annotationCloseBtn.addEventListener('click', function() {
     annotationWrapper.classList.remove('show-annotation');
     setTimeout(function() {
       annotationParagraph.innerHTML = '';
     }, 300);
-    annotationBtn.forEach(function(btn) {btn.setAttribute('aria-describedby', '')});
-    annotationWrapper.dataset.opener = 'none';
-    annotationBtn.forEach(function(btn) {btn.classList.remove('annotation-btn--active')});
+    annotationBtn.forEach(function(btn) {
+      btn.classList.remove('annotation-btn--active');
+      btn.setAttribute('aria-describedby', '');
+    });
   });
-  
+
 })();
